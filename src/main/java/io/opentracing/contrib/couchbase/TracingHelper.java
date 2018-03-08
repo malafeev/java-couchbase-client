357fa32bcd9f39f1096def1c0cb82698f2c084c6
Sergei Malafeev <sergeymalafeev@gmail.com>
2018-03-08 22:57:49 +0800

    add copyright

--- src/main/java/io/opentracing/contrib/couchbase/TracingHelper.java
+++ src/main/java/io/opentracing/contrib/couchbase/TracingHelper.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2018 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.opentracing.contrib.couchbase;
 
 import io.opentracing.Span;
@@ -91,5 +104,9 @@ public class TracingHelper {
     }
 
     return builder.toString();
+  }
+
+  public Tracer getTracer() {
+    return tracer;
   }
 }
